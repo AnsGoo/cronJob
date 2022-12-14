c2564d8eb3903d782dc770d6eec92dd152c3c69f
dengshangxing <sdeng@xadongda.com>
2022-12-14 11:23:24 +0800

    取消无用文件

--- scheduler/task.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-class BaseTask:
-    def methods(self) -> List[str]:
-        return (list(filter(lambda m: m.startswith("task") and not m.endswith("__") and callable(getattr(self, m)),
-                            dir(self))))
